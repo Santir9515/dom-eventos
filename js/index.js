9b16eed045ef20b3d0adc664d843f6d00c4b048c
Santiago Rodriguez <santir9515@gmail.com>
2022-08-10 20:06:34 -0300

    quinto commit

--- js/index.js
+++ js/index.js
@@ -9,17 +9,17 @@ class Alumnos {
 }
 
 let alumnosCoder = []
-const alumno1 = new Alumnos (1, "Santiago", "Rodriguez", "Masculino", 27)
+const alumno1 = new Alumnos (1, "Santiago", "Rodriguez", "Masculino", 27, "assets/Alumno1.jpg")
 alumnosCoder.push(alumno1)
-const alumno2 = new Alumnos (2, "Estefania", "Martinez", "Femenino", 26)
+const alumno2 = new Alumnos (2, "Estefania", "Martinez", "Femenino", 26, "assets/Alumna2.jpg")
 alumnosCoder.push(alumno2)
-const alumno3 = new Alumnos (3, "Nicolas", "Jaime", "Masculino", 30)
+const alumno3 = new Alumnos (3, "Nicolas", "Jaime", "Masculino", 30, "assets/Alumno6.jpg")
 alumnosCoder.push(alumno3)
-const alumno4 = new Alumnos (4, "Julieta", "Maidana", "Femenino", 25)
+const alumno4 = new Alumnos (4, "Julieta", "Maidana", "Femenino", 25, "assets/Alumna3.jpg")
 alumnosCoder.push(alumno4)
-const alumno5 = new Alumnos (5, "Vanina", "Franqui", "Femenino", 21)
+const alumno5 = new Alumnos (5, "Vanina", "Franqui", "Femenino", 21, "assets/Alumna4.jpg")
 alumnosCoder.push(alumno5)
-const alumno6 = new Alumnos (6, "Eugenia", "Leslie", "Femenino", 24)
+const alumno6 = new Alumnos (6, "Eugenia", "Leslie", "Femenino", 24, "assets/Alumna5.png")
 alumnosCoder.push(alumno6)
 
 
@@ -34,13 +34,13 @@ class Profesores {
 }
 
 let profesoresCoder = []
-const profesor1 = new Profesores (1, "Gloria", "Martinez", 36, "Desarrollo Web")
+const profesor1 = new Profesores (1, "Gloria", "Martinez", 36, "Desarrollo Web","assets/Profesora1.jpg")
 profesoresCoder.push(profesor1)
-const profesor2 = new Profesores (2, "Jose", "Rodriguez",31, "Javascript")
+const profesor2 = new Profesores (2, "Jose", "Rodriguez",31, "Javascript","assets/Profesor2.jpg")
 profesoresCoder.push(profesor2)
-const profesor3 = new Profesores (3, "Belen", "Escobar", 32, "React Js")
+const profesor3 = new Profesores (3, "Belen", "Escobar", 32, "React Js","assets/Profesora3.jpg")
 profesoresCoder.push(profesor3)
-const profesor4 = new Profesores (4, "Mateo", "Suarez", 28, "Programacion Backend")
+const profesor4 = new Profesores (4, "Mateo", "Suarez", 28, "Programacion Backend","assets/Profesor4.jpg")
 profesoresCoder.push(profesor4)
 
 function nuevoProfesor(){
